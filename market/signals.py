e67990de1f7137e40b4e09ffba28721f8a1bb9fe
ArinaKrish <lady.krishtafovich@mail.ru>
2023-07-30 16:50:13 +0300

    help

--- market/signals.py
+++ market/signals.py
@@ -10,7 +10,7 @@ new_order = Signal()
 
 
 
-@receiver(reset_password_token_created)
+@receiver(reset_password_token_created) # что значит собачка?
 def password_reset_token_created(sender, instance, reset_password_token, **kwargs):
     """
     Отправляем письмо с токеном для сброса пароля
